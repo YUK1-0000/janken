1421bb168d1241b516af211f9cec098fa190ffdf
YUK1 <tanaka543taro@gmail.com>
2024-06-13 20:56:29 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <random>
+
+bool search_num(int *num_array, int num)
+{
+	bool result = false;
+
+	for (int i = 0; i < (sizeof(*num_array) / sizeof(int)) + 1; i++)
+	{
+		if (num == num_array[i])
+		{
+			result = true;
+			break;
+		}
+	}
+
+	return result;
+}
+
+int janken(void)
+{
+	int allowed_nums[] = {1, 2, 3};
+	const char* hands[] = { "ぐー", "ちょき", "ぱー" };
+	int cpu_ind, player_ind;
+
+	while (true)
+	{
+		printf("\n1:ぐー, 2:ちょき, 3:ぱー\n");
+		scanf_s("%d", &player_ind);
+
+		if (search_num(allowed_nums, player_ind))
+		{
+			break;
+		}
+	}
+
+	player_ind -= 1;
+	cpu_ind = rand() % 3;
+
+	printf("\n自分: %s, 乱数: %s\n", hands[player_ind], hands[cpu_ind]);
+
+	if (player_ind == cpu_ind)
+	{
+		printf("引き分け");
+	}
+	else if (player_ind + 1 % 3 == cpu_ind)
+	{
+		printf("勝ち");
+	}
+	else
+	{
+		printf("負け");
+	}
+	printf("\n\n");
+
+	return 0;
+}
+
+int main(void)
+{
+	int allowed_nums[] = { 1, 2 };
+	int num;
+
+	while (true)
+	{
+		janken();
+
+		while (true)
+		{
+			printf("1:続ける, 2:やめる\n");
+			scanf_s("%d", &num);
+
+			if (search_num(allowed_nums, num))
+			{
+				break;
+			}
+		}
+
+		if (num == 2)
+		{
+			break;
+		}
+	}
+	return 0;
+}
+
